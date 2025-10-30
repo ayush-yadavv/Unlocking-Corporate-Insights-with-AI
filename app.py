764e779f81de119923d8572cb61b0d2844389425
ayush <yadav.ayushx1@gmail.com>
2025-10-30 21:00:45 +0530

    fix

--- app.py
+++ app.py
@@ -46,7 +46,9 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_core.retrievers import BaseRetriever
-from langchain.chains.retrieval_qa.base import BaseRetrievalQA as RetrievalQA
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import chromadb
